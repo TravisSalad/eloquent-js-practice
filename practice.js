a89bee42f64eae2079adf6fa20651ac483df7b83
Travis Salad <travissalad@Traviss-MacBook-Pro.local>
2016-07-06 09:52:39 -0700

    completed ch 4 problem 1

--- practice.js
+++ practice.js
@@ -70,8 +70,18 @@ function range(start, end, step) {
   if (step == undefined) {
     step = 1;
   }
-  if (start > end) {
+  else if ((start > end) && (step < 0)) {
+    for(var i = start; i >= end; i += step) {
+      result.push(i);
+    }
+  }
+  else if ((start > end) && (step > 0)) {
     for(var i = start; i >= end; i -= step) {
+      result.push(i);
+    }
+  }
+  else if ((start < end) && (step < 0)) {
+    for(var i = start; i <= end; i -= step) {
       result.push(i);
     }
   }
